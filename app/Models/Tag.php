ec855f74f45624e7b554dd76aa5c9caca59fbcc4
vagrant <vagrant@homestead>
2016-04-14 15:09:05 +0000

    created tag model and migration and tagstest

---
+++ app/Models/Tag.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Trackit\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    protected $fillable = ['name'];
+
+    public function proposals()
+    {
+    	return $this->belongsToMany(Proposal::class);
+    }
+
+}
