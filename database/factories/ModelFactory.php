ecebb87fab22a86c8baf0962960b1c36b7be5759
vagrant <vagrant@homestead>
2016-04-14 10:18:09 +0000

    defined tests for roles, started working on roles models

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,8 @@
 |
 */
 
-$factory->define(Trackit\User::class, function (Faker\Generator $faker) {
+$factory->define(Trackit\Models\Role::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
     ];
 });
