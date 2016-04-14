3d1130ee4768079377a58a4f7b363b4c2bc3bbb6
kallexander <kalle.bornemark@gmail.com>
2016-04-14 11:07:41 +0000

    forgot to add files

---
+++ app/Models/Role.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Trackit\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public function scopeByName($query, $name)
+    {
+        return $query->where('name', $name);
+    }
+
+    public function can($action)
+    {
+        return !! $this->permissions()
+                       ->where('name', $action)
+                       ->first();
+    }
+
+    public function permissions()
+    {
+        return $this->hasMany(Permission::class);
+    }
+}
---
+++ tests/integration/models/RolesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Trackit\Models\Role;
+
+class RolesTest extends TestCase
+{
+    use DatabaseMigrations;
+    /** @test */
+    public function it_has_a_set_of_permissions()
+    {
+        // Given
+        // $role = Role::byName('customer')->first();
+        $role = factory(Role::class)->create(['name' => 'customer']);
+
+        // When
+        $canSubmit = $role->can('submit');
+        // $canView = $role->canView();
+        // $canPublish = $role->canPublish();
+        // $canSearch = $role->canSearch();
+        // $canApprove = $role->canApprove();
+
+        // Then
+        $this->assertTrue($canSubmit);
+        // $this->assertTrue($canView);
+        // $this->assertFalse($canPublish);
+        // $this->assertFalse($canSearch);
+        // $this->assertFalse($canApprove);
+    }
+}
