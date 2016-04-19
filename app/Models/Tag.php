9d47b8628346123cf3580d26ceb39a9552cc8924
JimmyLindstrom <jimmylindstrom79@gmail.com>
2016-04-19 12:29:40 +0200

    fixed review issues

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = [
+    	'name',
+    ];
 
     public function proposals()
     {
