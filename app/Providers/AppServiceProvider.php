676f30590bcfab9975436f455d8067e06ddba072
nekosaur <albert@kaaman.nu>
2016-04-21 23:29:04 +0200

    Removed unnecessary contextual binding from service provider Removed unnecessary ProposalAttachmentController

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app
-            ->when(Trackit\Http\Controller\ProposalAttachmentController::class)
-            ->needs(Trackit\Contracts\Attachmentable::class)
-            ->give(Trackit\Models\Proposal::class);
+        //
     }
 }
