f249ebc46103347bb9c94e889e916d6fed3f18a8
nekosaur <albert@kaaman.nu>
2016-04-25 14:49:46 +0200

    Added proposals and role relationships to User Added refreshApiToken method User automatically adds api token when created

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,4 +9,29 @@ class User extends Model
     protected $fillable = [
         'name',
     ];
+
+    public static function boot()
+    {
+    	parent::boot();
+
+    	static::created(function ($user) {
+    		$user->refreshApiToken();
+    	});
+    }
+
+    public function refreshApiToken()
+    {
+    	$this->api_token = str_random(128);
+    	$this->save();
+    }
+
+    public function proposals()
+    {
+    	return $this->hasMany(Proposal::class);
+    }
+
+    public function role()
+    {
+    	return $this->belongsTo(Role::class);
+    }
 }
