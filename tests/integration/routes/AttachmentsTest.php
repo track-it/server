bb9ae31bcb943addfc45f95991c0454e2aa3b227
nekosaur <albert@kaaman.nu>
2016-04-26 13:09:34 +0200

    Added tests for attachemnts

--- tests/integration/routes/AttachmentsTest.php
+++ tests/integration/routes/AttachmentsTest.php
@@ -12,22 +12,9 @@ class AttachmentsTest extends TestCase
     use DatabaseTransactions;
 
     /** @test */
-    public function is_should_return_an_existing_attachment()
-    {
-        $attachment = factory(Attachment::class)->create();
-        $proposal = factory(Proposal::class)->create();
-        $proposal->attachments()->save($attachment);
-        $proposal->save();
-
-        $response = $this->get('proposals/'.$proposal->id.'/attachments/'.$attachment->id)->response;
-        $jsonObject = json_decode($response->getContent());
-
-        $this->assertEquals($attachment->id, $jsonObject->items[0]->id);
-    }
-
-    /** @test */
     public function it_should_upload_an_attached_file()
     {
+        $proposal = factory(Proposal::class)->create();
         $file = new UploadedFile(
             base_path('tests/files/test.txt'), 
             'test.txt',
@@ -37,8 +24,6 @@ class AttachmentsTest extends TestCase
             false
         );
 
-        $proposal = factory(Proposal::class)->create();
-
         $response = $this->call(
             'POST',
             'proposals/'.$proposal->id.'/attachments',
@@ -46,9 +31,76 @@ class AttachmentsTest extends TestCase
             [],
             [$file]
         );
-
         $jsonObject = json_decode($response->getContent());
 
         $this->assertEquals($file->getClientOriginalName(), $jsonObject->items[0]->title);
+    }
+
+    /** @test */
+    public function is_should_return_an_existing_attachment()
+    {
+        $attachment = factory(Attachment::class)->create();
+
+        $response = $this->get('attachments/'.$attachment->id)->response;
+        $jsonObject = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals($attachment->id, $jsonObject->items[0]->id);
+    }
+
+    /** @test */
+    public function is_should_update_an_existing_attachment()
+    {
+        $attachment = factory(Attachment::class)->create();
+        $data = [
+            'title' => 'New Title',
+        ];
+
+        $response = $this->put('attachments/'.$attachment->id, $data)->response;
+        $jsonObject = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals($data['title'], $jsonObject->items[0]->title);
+    }
+
+    /** @test */
+    public function is_should_delete_an_existing_attachment()
+    {
+        $attachment = factory(Attachment::class)->create();
+
+        $response = $this->delete('attachments/'.$attachment->id)->response;
+
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function is_should_download_an_attached_file()
+    {
+        $attachment = factory(Attachment::class)->create();
+        $attachment->url = 'attachments/'.$attachment->id.'/test.txt';
+        $content = file_get_contents(getcwd() . '/tests/files/test.txt');
+        Storage::put($attachment->url, $content);
+        $attachment->save();
+
+        $response = $this->get('attachments/'.$attachment->id.'/download')->response;
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals($content, $response->getContent());
+    }
+
+    /** @test */
+    public function is_should_return_all_attachments_for_a_proposal()
+    {
+        $proposal = factory(Proposal::class)->create();
+        $attachment = factory(Attachment::class)->create();
+        $attachment2 = factory(Attachment::class)->create();
+        $proposal->attachments()->save($attachment);
+        $proposal->attachments()->save($attachment2);
+
+        $response = $this->get('proposals/'.$proposal->id.'/attachments/')->response;
+        $jsonObject = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(2, count($jsonObject->items));
     }
 }
