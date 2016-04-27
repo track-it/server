b40b8a24dc0df1bce7811d87b9bf54afa169cb61
JimmyMaksymiw <jimmy.maksymiw@gmail.com>
2016-04-27 23:08:30 +0200

    added tests for comment routes

--- tests/integration/routes/CommentsTest.php
+++ tests/integration/routes/CommentsTest.php
@@ -29,23 +29,63 @@ class CommentsTest extends TestCase
 	}
 
 	/** @test */
-	public function it_should_show_all_comments_on_a_commentable_resource()
+	public function it_should_return_all_comments_on_a_commentable_resource()
 	{
 		$proposal = factory(Proposal::class)->create();
 		$user = factory(User::class)->create();
 
 		$url = 'proposals/'.$proposal->id.'/comments';
-		$content1 = ['body' => 'This is a body.', 'author_id' => $user->id];
-		$content2 = ['body' => 'This is another body.', 'author_id' => $user->id];
 		$header = $this->createAuthHeader();
 
-		$this->get($url, $content1, $header);
-		$this->get($url, $content2, $header);
+		$content1 = ['body' => 'This is a body.', 'author_id' => $user->id];
+		$content2 = ['body' => 'This is another body.', 'author_id' => $user->id];
+
+		$this->post($url, $content1, $header);
+		$this->post($url, $content2, $header);
 
 		$response = $this->get($url, $header)->response;
 		$jsonObject = json_decode($response->getContent());
 
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(2, $jsonObject->items->count());
+        $this->assertEquals(2, count($jsonObject->items));
 	}
+
+	/** @test */
+	public function it_should_return_a_failure_for_a_non_existing_resource()
+	{
+		$user = factory(User::class)->create();
+		$url = 'proposals/9999999999999/comments';
+		$content = ['body' => 'This is a body.', 'author_id' => $user->id];
+		$header = $this->createAuthHeader();
+
+        $response = $this->post($url, $content, $header)->response;
+		
+        $this->assertEquals(404, $response->getStatusCode());
+	}
+
+	/** @test */
+    public function it_should_delete_an_existing_comment_on_a_commentable_resource()
+    {
+        $comment = factory(Comment::class)->create();
+        $header = $this->createAuthHeader();
+
+        $response = $this->delete('comments/'.$comment->id, [], $header)->response;
+
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+	/** @test */
+    public function it_should_update_an_existing_comment_on_a_commentable_resource()
+    {
+        $comment = factory(Comment::class)->create();
+        $data = ['body' => 'new body'];
+
+        $header = $this->createAuthHeader();
+
+        $response = $this->put('comments/'.$comment->id, $data, $header)->response;
+        $jsonObject = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals($data['body'], $jsonObject->items[0]->body);
+    }
 }
