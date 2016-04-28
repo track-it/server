095787d709444aae5df08c0366f7dcc0739ff307
JimmyMaksymiw <jimmy.maksymiw@gmail.com>
2016-04-28 14:36:34 +0200

    Added project migration

---
+++ database/migrations/2016_04_28_125214_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('proposal_id')->unsigned()->index();
+            $table->integer('team_id')->unsigned()->index();
+            $table->integer('course_id')->unsigned()->index();
+            $table->integer('status')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+    }
+}
