e953f81bc13995ad84b12c91edaa814d0fc54027
kallexander <kalle.bornemark@gmail.com>
2016-04-29 15:50:34 +0200

    Wrote authorization tests for UpdateProposalRequest

--- tests/integration/requests/UpdateProposalRequestTest.php
+++ tests/integration/requests/UpdateProposalRequestTest.php
@@ -5,115 +5,48 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 use Trackit\Models\Proposal;
+use Trackit\Models\User;
 
 class UpdateProposalRequestTest extends TestCase
 {
     use DatabaseTransactions;
 
-    /** @test */
-    public function it_should_not_allow_a_title_longer_than_100_characters()
-    {
-        $proposal = factory(Proposal::class)->create();
-        $header = $this->createAuthHeader();
-        $data = [
-            'title' => str_random(1000),
-            'description' => 'This is a description',
-        ];
-
-        $response = $this->json('PUT', 'proposals/'.$proposal->id, $data, $header)->response;
-        $jsonObject = json_decode($response->getContent());
-
-        $this->assertEquals(422, $response->getStatusCode());
-        $this->assertEquals('The title may not be greater than 100 characters.', $jsonObject->title[0]);
-    }
+    // -------------------------------------------------------------------
+    //                          AUTHORIZATION
+    // -------------------------------------------------------------------
 
     /** @test */
-    public function it_should_not_allow_a_missing_title()
-    {
-        $proposal = factory(Proposal::class)->create();
-        $header = $this->createAuthHeader();
-        $data = [
-            'description' => 'This is a description',
-        ];
-
-        $response = $this->json('PUT', 'proposals/'.$proposal->id, $data, $header)->response;
-        $jsonObject = json_decode($response->getContent());
-
-        $this->assertEquals(422, $response->getStatusCode());
-        $this->assertEquals('The title field is required.', $jsonObject->title[0]);
-    }
-
-    /** @test */
-    public function it_should_not_allow_a_description_longer_than_5000_characters()
-    {
-        $proposal = factory(Proposal::class)->create();
-        $header = $this->createAuthHeader();
-        $data = [
-            'title' => 'This is a title',
-            'description' => str_random(5001),
-        ];
-
-        $response = $this->json('PUT', 'proposals/'.$proposal->id, $data, $header)->response;
-        $jsonObject = json_decode($response->getContent());
-
-        $this->assertEquals(422, $response->getStatusCode());
-        $this->assertEquals('The description may not be greater than 5000 characters.', $jsonObject->description[0]);
-    }
-
-    /** @test */
-    public function it_should_not_allow_a_missing_description()
-    {
-        $proposal = factory(Proposal::class)->create();
-        $header = $this->createAuthHeader();
-        $data = [
-            'title' => 'This is a title',
-        ];
-
-        $response = $this->json('PUT', 'proposals/'.$proposal->id, $data, $header)->response;
-        $jsonObject = json_decode($response->getContent());
-
-        $this->assertEquals(422, $response->getStatusCode());
-        $this->assertEquals('The description field is required.', $jsonObject->description[0]);
-    }
-
-    /** @test */
-    public function it_should_not_allow_tags_not_in_an_array()
-    {
-        $proposal = factory(Proposal::class)->create();
+    public function it_should_allow_an_author_to_update()
+    {        
+        $proposal = factory(Proposal::class)->create(['user_id' => $this->getUser()->id]);
         $header = $this->createAuthHeader();
         $data = [
             'title' => 'This is a title',
             'description' => 'This is a description',
-            'tags' => 'test',
         ];
 
         $response = $this->json('PUT', 'proposals/'.$proposal->id, $data, $header)->response;
         $jsonObject = json_decode($response->getContent());
 
-        $this->assertEquals(422, $response->getStatusCode());
-        $this->assertEquals('The tags must be an array.', $jsonObject->tags[0]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('This is a title', $jsonObject->data->title);
     }
 
     /** @test */
-    public function it_should_not_allow_more_than_20_tags()
+    public function it_should_disallow_a_non_author_to_update()
     {
-        $proposal = factory(Proposal::class)->create();
-        $faker = Faker\Factory::create();
-        $tags = [];
-        for ($i = 0; $i < 21; $i++) {
-            $tags[] = $faker->word();
-        }
-        $header = $this->createAuthHeader();
+        $proposal = factory(Proposal::class)->create(['user_id' => $this->getUser()->id]);
+        $originalTitle = $proposal->title;
         $data = [
             'title' => 'This is a title',
             'description' => 'This is a description',
-            'tags' => $tags,
         ];
+        $nonAuthorHeader = $this->createAuthHeader();
 
-        $response = $this->json('PUT', 'proposals/'.$proposal->id, $data, $header)->response;
+        $response = $this->json('PUT', 'proposals/'.$proposal->id, $data, $nonAuthorHeader)->response;
         $jsonObject = json_decode($response->getContent());
 
-        $this->assertEquals(422, $response->getStatusCode());
-        $this->assertEquals('The tags may not have more than 20 items.', $jsonObject->tags[0]);
+        $this->assertEquals(403, $response->getStatusCode());
+        $this->assertEquals($originalTitle, $proposal->title);
     }
 }
