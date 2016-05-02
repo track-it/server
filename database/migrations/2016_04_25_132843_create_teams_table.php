7fcfe0c522b3ef270d40a67076250f7ebaff5f53
vagrant <vagrant@homestead>
2016-05-02 12:18:08 +0000

    Deleted relationship with course model, updated test

--- database/migrations/2016_04_25_132843_create_teams_table.php
+++ database/migrations/2016_04_25_132843_create_teams_table.php
@@ -14,9 +14,6 @@ class CreateTeamsTable extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->increments('id');
-
-            $table->string('course');
-
             $table->timestamps();
         });
     }
