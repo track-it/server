b4db055f989bae2cbc7840e50f2e6e3562871693
nekosaur <albert@kaaman.nu>
2016-05-03 14:19:43 +0200

    Added more information to incorrect authentication attempts

--- tests/integration/routes/AuthenticationTest.php
+++ tests/integration/routes/AuthenticationTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class AuthenticationTest extends TestCase
 {
-	use DatabaseTransactions;
+    use DatabaseTransactions;
 
     /** @test */
     public function it_should_allow_user_to_log_in_with_valid_credentials()
@@ -14,12 +14,12 @@ class AuthenticationTest extends TestCase
         $user = factory(Trackit\Models\User::class)->create(['password' => 'nisse']);
 
         $data = [
-        	'username' 	=> $user->username,
-        	'password'  => 'nisse',
+            'username'  => $user->username,
+            'password'  => 'nisse',
         ];
 
         $server = [
-        	'X-CSRF-TOKEN'	=> csrf_token(),
+            'X-CSRF-TOKEN'  => csrf_token(),
         ];
 
         $response = $this->post('/auth/login', $data, $server)->response;
@@ -31,21 +31,43 @@ class AuthenticationTest extends TestCase
     }
 
     /** @test */
-    public function it_should_return_error_using_incorrect_credentials()
+    public function it_should_return_error_when_using_incorrect_credentials()
     {
-		$user = factory(Trackit\Models\User::class)->create(['password' => 'nisse']);
+        $user = factory(Trackit\Models\User::class)->create(['password' => 'nisse']);
 
         $data = [
-        	'username'	=> $user->username,
-        	'password'  => 'olle',
+            'username'  => $user->username,
+            'password'  => 'olle',
         ];
 
         $server = [
-        	'X-CSRF-TOKEN'	=> csrf_token(),
+            'X-CSRF-TOKEN'  => csrf_token(),
         ];
 
         $response = $this->post('/auth/login', $data, $server)->response;
+        $jsonObject = json_decode($response->getContent());
 
         $this->assertEquals(401, $response->getStatusCode());
+        $this->assertEquals('Incorrect password.', $jsonObject->error);
+    }
+
+    /** @test */
+    public function it_should_return_error_when_user_does_not_exist()
+    {
+
+        $data = [
+            'username'  => 'olle',
+            'password'  => 'olle',
+        ];
+
+        $server = [
+            'X-CSRF-TOKEN'  => csrf_token(),
+        ];
+
+        $response = $this->post('/auth/login', $data, $server)->response;
+        $jsonObject = json_decode($response->getContent());
+
+        $this->assertEquals(401, $response->getStatusCode());
+        $this->assertEquals('Unknown username.', $jsonObject->error);
     }
 }
