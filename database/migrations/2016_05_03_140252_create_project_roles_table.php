47b01ddb673bd62a727213932088ac9eca95485a
JimmyMaksymiw <jimmy.maksymiw@gmail.com>
2016-05-03 17:28:51 +0200

    added migration for project roles

---
+++ database/migrations/2016_05_03_140252_create_project_roles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('project_roles');
+    }
+}
