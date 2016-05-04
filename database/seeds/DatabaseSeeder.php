47a322947069e35f011302c1f90ffc4fe64ff2fd
JimmyMaksymiw <jimmy.maksymiw@gmail.com>
2016-05-04 15:04:02 +0200

    updated Role and Permission seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
     	$this->call(RolesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
+        $this->call(PermissionRoleTableSeeder::class);
     }
 }
