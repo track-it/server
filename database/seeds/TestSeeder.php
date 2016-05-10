cc7d46bd0e8ae507ac5325b71aa487f98464ed0b
kallexander <kalle.bornemark@gmail.com>
2016-05-10 21:12:30 +0200

    Added students as projects users in project

--- database/seeds/TestSeeder.php
+++ database/seeds/TestSeeder.php
@@ -102,6 +102,9 @@ class TestSeeder extends Seeder
 
         $project1->proposal()->associate($proposal1)->save();
         $project1->team()->associate($team)->save();
+        $project1->addProjectUser('student', $student1);
+        $project1->addProjectUser('student', $student2);
+        $project1->addProjectUser('student', $student3);
         $project1->addProjectUser('teacher', $teacher);
         $project2->addProjectUser('teacher', $teacher);
         $project1->addProjectUser('stakeholder', $customer);
