f5d4b73cb062b2ac9fa5c8bdf719c463aae5facd
kallexander <kalle.bornemark@gmail.com>
2016-05-10 14:48:29 +0200

    Added TestSeeder

--- database/seeds/TestSeeder.php
+++ database/seeds/TestSeeder.php
@@ -8,6 +8,7 @@ use Trackit\Models\Project;
 use Trackit\Models\Attachment;
 use Trackit\Models\Team;
 use Trackit\Models\Role;
+use Trackit\Models\Comment;
 
 class TestSeeder extends Seeder
 {
@@ -29,6 +30,8 @@ class TestSeeder extends Seeder
             'role_id' => Role::byName('teacher')->first()->id,
         ]);
         $student1 = factory(User::class)->create([
+            'username' => 'student',
+            'password' => 'password',
             'role_id' => Role::byName('student')->first()->id,
         ]);
         $student2 = factory(User::class)->create([
@@ -47,9 +50,20 @@ class TestSeeder extends Seeder
 
         $proposal1 = factory(Proposal::class)->create([
             'author_id' => $customer->id,
+            'status' => Proposal::NOT_REVIEWED,
         ]);
-        $proposal2 = factory(Proposal::class)->create();
-        $proposal3 = factory(Proposal::class)->create();
+        $proposal2 = factory(Proposal::class)->create([
+            'status' => Proposal::UNDER_REVIEW,
+        ]);
+        $proposal3 = factory(Proposal::class)->create([
+            'status' => Proposal::NOT_APPROVED,
+        ]);
+        $proposal4 = factory(Proposal::class)->create([
+            'status' => Proposal::APPROVED,
+        ]);
+        $proposal5 = factory(Proposal::class)->create([
+            'status' => Proposal::ARCHIVED,
+        ]);
 
         $project1 = factory(Project::class)->create();
         $project2 = factory(Project::class)->create();
@@ -59,16 +73,32 @@ class TestSeeder extends Seeder
         $proposal1->attachments()->save($attachment2);
         $proposal1->attachments()->save($attachment3);
 
+        $comment1 = factory(Comment::class)->create();
+        $comment2 = factory(Comment::class)->create();
+        $comment3 = factory(Comment::class)->create();
+        $comment4 = factory(Comment::class)->create();
+
+        $comment1->author()->associate($student1)->save();
+        $comment2->author()->associate($teacher)->save();
+        $comment3->author()->associate($customer)->save();
+        $comment4->author()->associate($admin)->save();
+
         $team = factory(Team::class)->create();
         $student1->joinTeam($team);
         $student2->joinTeam($team);
         $student3->joinTeam($team);
 
-        $proposal1->
+        $proposal1->comments()->save($comment1);
+        $proposal1->comments()->save($comment2);
+        $proposal1->comments()->save($comment3);
 
-        $project1->proposal()->associate($proposal1);
-        $project1->team()->associate($team);
+        $project1->proposal()->associate($proposal1)->save();
+        $project1->team()->associate($team)->save();
         $project1->addProjectUser('teacher', $teacher);
-        $project1->addProjectUser('steakholder', $customer);
+        $project1->addProjectUser('stakeholder', $customer);
+
+        $project1->attachments()->save($attachment1);
+        $project1->attachments()->save($attachment2);
+        $project1->attachments()->save($attachment3);
     }
 }
