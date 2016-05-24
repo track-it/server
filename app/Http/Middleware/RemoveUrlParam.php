b8f5b7b10fffb5aaec791e83611471a77174ebd8
www-data <www-data@trackit>
2016-05-24 12:11:44 -0400

    Added fix middleware

--- app/Http/Middleware/RemoveUrlParam.php
+++ app/Http/Middleware/RemoveUrlParam.php
@@ -8,7 +8,11 @@ use Illuminate\Support\Facades\Auth;
 class RemoveUrlParam
 {
     /**
-     * Handle an incoming request.
+     * This is really only a temporary solution for running Laravel in a sub-folder.
+     * We're rewriting urls in nginx to index.php?$args, which means that the url
+     * will be a query parameters, which screws with some of our logic.
+     *
+     * Here we simply try to find the request url and remove it from query parameters.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
@@ -17,6 +21,13 @@ class RemoveUrlParam
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        dd($request);
+	foreach ($request->all() as $key => $value) {
+	    $url = explode("?", $request->server('REQUEST_URI'))[0];
+	    if (strpos($url, $key) !== false) {
+		$request->query->remove($key);
+	    }
+	}
+
+	return $next($request);
     }
 }
